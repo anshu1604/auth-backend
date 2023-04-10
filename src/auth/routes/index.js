4c88f2eb462c1efad95ad453c3c442c0c9b85a9d
anshu1604 <engineeranshu1999@gmail.com>
2023-04-10 21:28:30 +0530

    Access token verified with token saved in DB

--- src/auth/routes/index.js
+++ src/auth/routes/index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import Validations from "../../utils/Validations.js";
 import AuthController from "../controllers/AuthController.js";
+import { verifyToken } from "../../utils/verifyToken.js";
 
 const authRouter = express.Router();
 
-authRouter.put("/auth", Validations.registerValidation(), AuthController.save);
+authRouter.put("/user", [Validations.registerValidation(), verifyToken], AuthController.save);
 
 export default authRouter;
